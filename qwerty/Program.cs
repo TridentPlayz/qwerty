2d30910b634d5c9d89b5f6911bb17a3f9c8c658f
tristen <tristenRN@gmail.com>
2021-11-10 13:25:01 +0200

    Add project files.

---
+++ qwerty/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace qwerty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Its a qwerty keyborad app");
+            Console.ReadLine();
+        }
+    }
+}
